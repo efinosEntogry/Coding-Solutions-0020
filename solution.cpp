c07738853e39a8c23dfcd0e74fb67b2634622141
efinosEntogry <165686302+efinosEntogry@users.noreply.github.com>
2025-10-26 08:18:44 +0500

    solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if (n == 1 ) return true;
+    if (n <= 0 || n % 2 != 0) return false;
+    return isPowerOfTwo(n / 2);
+}
+
+int main() {
+
+    cout << boolalpha;
+    cout << isPowerOfTwo(12) << endl;
+
+    return 0;
+}
